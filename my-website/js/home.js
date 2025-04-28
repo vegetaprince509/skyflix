63887e0e8e80c60c7599764428d65cfec3a6e28e
vegetaprince509 <vegetaprince509@gmail.com>
2025-04-28 09:32:37 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '330f50bce922dedb5512f27b88ddeed4';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
